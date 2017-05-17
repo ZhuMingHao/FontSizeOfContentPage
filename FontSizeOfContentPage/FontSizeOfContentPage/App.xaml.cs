d25cd81745f5455f7fce7edcb17564595230a734
Nico Zhu (Shang Hai Wei Chuang Ruan Jian) <v-zhumin@microsoft.com>
2017-05-17 12:55:37 +0800

    Add project files.

---
+++ FontSizeOfContentPage/FontSizeOfContentPage/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace FontSizeOfContentPage
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            var nav = new NavigationPage(new MainPage());
+            nav.Title = "ddddd";
+            MainPage = nav;
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
